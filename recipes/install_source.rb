59d33e2ee314f8ecfbe7451ca92d36d088006b27
Yann ROBIN <me.show@gmail.com>
2013-10-11 02:00:12 +0200

    clean version
    clean version but still not working (may need some help on this)

--- recipes/install_source.rb
+++ recipes/install_source.rb
@@ -1,11 +1,14 @@
 include_recipe "build-essential"
 include_recipe "ark"
 
+package "libpopt-dev"
+
 is_installed_command = "keepalived -v 2>&1 >/dev/null | grep -qF #{node['keepalived']['version']}"
 
-ark "Install keepalived from sources" do
+ark "keepalived" do
   url "http://www.keepalived.org/software/keepalived-#{node['keepalived']['version']}.tar.gz"
   autoconf_opts node['keepalived']['config_opts'] if node['keepalived']['config_opts']
-  action :install_with_make
   not_if is_installed_command
+  version node['keepalived']['version']
+  action [ :install ]
 end
