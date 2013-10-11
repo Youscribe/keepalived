eaf685155ffddd402f2bc2711ae1f7fd8a606508
Yann <yann.robin@youscribe.com>
2013-10-11 11:57:13 +0200

    add install ppa
    add install ppa and pin package

---
+++ recipes/install_ppa.rb
@@ -0,0 +1,18 @@
+apt_repository "keepalived" do
+  uri "http://ppa.launchpad.net/keepalived/stable/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "7C33BDC6"
+end
+
+ # needed because of "epoch" in default Package and launchpad can't build with epoch
+ # https://bugs.launchpad.net/ubuntu/+source/bzr-builder/+bug/613942
+ apt_preference 'pin-keepalived-ppa' do
+   glob         '*'
+   pin          'release o=LP-PPA-keepalived-stable'
+   pin_priority '1001'
+ end
+end
+
+package "keepalived"
