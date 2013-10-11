74aa40e922cfa3623f6f8bb4f370379dcddec62d
Yann <yann.robin@youscribe.com>
2013-10-11 12:17:03 +0200

    fix typo issue

--- recipes/install_ppa.rb
+++ recipes/install_ppa.rb
@@ -8,11 +8,11 @@ end
 
  # needed because of "epoch" in default Package and launchpad can't build with epoch
  # https://bugs.launchpad.net/ubuntu/+source/bzr-builder/+bug/613942
- apt_preference 'pin-keepalived-ppa' do
-   glob         '*'
-   pin          'release o=LP-PPA-keepalived-stable'
-   pin_priority '1001'
- end
+apt_preference 'pin-keepalived-ppa' do
+  glob         '*'
+  pin          'release o=LP-PPA-keepalived-stable'
+  pin_priority '1001'
 end
+
 
 package "keepalived"
